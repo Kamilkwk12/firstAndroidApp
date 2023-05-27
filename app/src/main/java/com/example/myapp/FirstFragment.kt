6862f0f4e46c2f24ab457fad0ab374c65a286419
Kamil Kwiatkowski <kamil.kwiatkowski2684@gmail.com>
2023-05-27 20:04:17 +0200

    two fragments in one activity

---
+++ app/src/main/java/com/example/myapp/FirstFragment.kt
@@ -0,0 +1,40 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.myapp.databinding.FragmentFirstBinding
+import com.example.myapp.databinding.FragmentSecondBinding
+
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentFirstBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.firstBtn.setOnLongClickListener {
+            Toast.makeText(requireContext(), "Przycisk 1 - długie naciśnięcie", Toast.LENGTH_SHORT).show()
+            true
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -18,18 +19,25 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.fragment.app.commit
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapp.databinding.MainLayoutBinding
 import com.example.myapp.ui.theme.MyAppTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+        supportFragmentManager.commit {
+            addToBackStack(null)
+            add(R.id.firstContainer, firstFragment, null )
+            add(R.id.secondContainer, secondFragment, null )
+        }
     }
 }
 
